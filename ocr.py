c393f7150458f3a2d2c7edf851c9c5391319f294
Sabbir Ahmed <sabbiirr@gmail.com>
2014-08-11 05:11:10 +0600

    Extract

--- ocr.py
+++ ocr.py
@@ -18,15 +18,26 @@ def ReadFile(file_name):
 
 def Extract(client):
     print "Extracting config file..."
-
+    ca = re.search('<ca>(.*)<\/ca>', client, flags=re.S ).group(1)
+    cert = re.search('<cert>(.*)<\/cert>', client, flags=re.S ).group(1)
+    key = re.search('<key>(.*)<\/key>', client, flags=re.S ).group(1)
+    ta = re.search('<tls-auth>(.*)<\/tls-auth>', client, flags=re.S ).group(1)
+    files = [ca, cert, key, ta]
+    return files
     
 
-def WriteFiles(**files):
+def WriteFiles(files):
     print "Writing configarations..."
-
+    for file in files:
+        print file, '\n'
+    return files
 
 try:
     client = ReadFile(config_file)
+    files = Extract(client)
+    WriteFiles(files)
+
 except Exception, e:
     print "Error : ", e
+
 
